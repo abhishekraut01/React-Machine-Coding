5f352482aecef9c95819f33561aa43834e2806e3
Abhishek <Abhishekraut8697@gmail.com>
2025-08-17 00:15:34 +0530

    completed ui and adding functionality

--- chips-input/src/components/ContentBox.tsx
+++ chips-input/src/components/ContentBox.tsx
@@ -1,12 +1,37 @@
+import { useState, type ChangeEvent } from "react"
 import Chips from "./Chips"
 
+const ChipsData = [
+    {
+        
+    }
+]
 
 const ContentBox = () => {
+    const [textData, setTextData] = useState("")
+
+    const handleChnage = (e: any) => {
+        e.preventDefault()
+        setTextData(e.target.value)
+        
+    }
+
     return (
         <div className="flex flex-col h-1/2 w-1/2 bg-red-100 rounded-md p-5">
-            <input className="py-3 px-3 mb-3 rounded" type="text" placeholder="Enter here" />
-            <div className="h-full w-full flex flex-wrap rounded-md bg-green-100">
-                <Chips/>
+            <input
+                className="py-3 px-3 mb-3 rounded"
+                type="text"
+                placeholder="Enter here"
+                value={textData}
+                onChange={(e) => handleChnage(e)}
+                onSubmit={()=>{
+                    setTextData("")
+                }}
+            />
+
+            <div className="h-full p-5  gap-5 overflow-x-hidden overflow-y-scroll w-full flex flex-wrap rounded-md bg-green-100">
+                <Chips />
+
             </div>
         </div>
     )
