26073b1e78e8c1a805f9aecef79989687bf5e23d
Abhishek <Abhishekraut8697@gmail.com>
2025-08-16 23:59:41 +0530

    added chips

---
+++ chips-input/src/components/Chips.tsx
@@ -0,0 +1,10 @@
+const Chips = () => {
+  return (
+    <div className="flex gap-5 justify-center items-center bg-red-400 h-8 w-40 rounded-md">
+        <h3>Hell i am text</h3>
+        <button className="cross-btn">X</button>
+    </div>
+  )
+}
+
+export default Chips
--- chips-input/src/components/ContentBox.tsx
+++ chips-input/src/components/ContentBox.tsx
@@ -1,14 +1,15 @@
+import Chips from "./Chips"
 
 
 const ContentBox = () => {
-  return (
-    <div className="flex flex-col h-1/2 w-1/2 bg-red-100 rounded-md">
-        <input type="text" />
-        <div className="h-full w-full flex flex-wrap">
-
+    return (
+        <div className="flex flex-col h-1/2 w-1/2 bg-red-100 rounded-md p-5">
+            <input className="py-3 px-3 mb-3 rounded" type="text" placeholder="Enter here" />
+            <div className="h-full w-full flex flex-wrap rounded-md bg-green-100">
+                <Chips/>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ContentBox
