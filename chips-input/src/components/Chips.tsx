1919af8e02efdaadc58abbf02bd496a35ab1ee88
Abhishek <Abhishekraut8697@gmail.com>
2025-08-17 01:08:03 +0530

    problem solved

--- chips-input/src/components/Chips.tsx
+++ chips-input/src/components/Chips.tsx
@@ -1,8 +1,14 @@
-const Chips = () => {
+interface IChipsProp{
+    text:string
+    id:number
+    handleRemove :(id:number)=>void
+}
+
+const Chips:React.FC<IChipsProp> = ({text , id  , handleRemove}) => {
   return (
     <div className="flex gap-5 p-3 justify-center items-center bg-red-400 h-8 w-auto rounded-md">
-        <h3>Hell i am text</h3>
-        <button className="cross-btn bg-blue-200 rounded-md px-3">X</button>
+        <h3>{text}</h3>
+        <button onClick={()=>handleRemove(id)} className="text-red cross-btn bg-blue-200 rounded-md px-3">X</button>
     </div>
   )
 }
--- chips-input/src/components/ContentBox.tsx
+++ chips-input/src/components/ContentBox.tsx
@@ -1,37 +1,61 @@
 import { useState, type ChangeEvent } from "react"
 import Chips from "./Chips"
+interface Ichips {
+    id: number,
+    text: string
+}
 
-const ChipsData = [
-    {
-        
-    }
+
+const ChipsData: Ichips[] = [
+
 ]
 
 const ContentBox = () => {
     const [textData, setTextData] = useState("")
+    const [chips, setChips] = useState<Ichips[]>(ChipsData)
 
-    const handleChnage = (e: any) => {
-        e.preventDefault()
+
+    const handleChnage = (e: ChangeEvent<HTMLInputElement>) => {
         setTextData(e.target.value)
-        
+    }
+
+    const handleAdd = (e: React.FormEvent) => {
+        e.preventDefault()
+        setChips((prev)=> [...prev , {
+            id:Date.now(),
+            text:textData
+        }])
+        setTextData("")
+    }
+
+    const handleRemove = (id: number) => {
+        setChips(chips.filter(x => x.id !== id)) 
     }
 
     return (
         <div className="flex flex-col h-1/2 w-1/2 bg-red-100 rounded-md p-5">
-            <input
-                className="py-3 px-3 mb-3 rounded"
-                type="text"
-                placeholder="Enter here"
-                value={textData}
-                onChange={(e) => handleChnage(e)}
-                onSubmit={()=>{
-                    setTextData("")
-                }}
-            />
+            <form onSubmit={handleAdd}>
+                <input
+                    className="py-3 px-3 mb-3 rounded w-full"
+                    type="text"
+                    placeholder="Enter here"
+                    value={textData}
+                    onChange={(e) => handleChnage(e)}
+
+                />
+            </form>
 
             <div className="h-full p-5  gap-5 overflow-x-hidden overflow-y-scroll w-full flex flex-wrap rounded-md bg-green-100">
-                <Chips />
-
+                {
+                    chips.map((chip) => {
+                        return <Chips
+                            key={chip.id}
+                            text={chip.text}
+                            id={chip.id}
+                            handleRemove={handleRemove}
+                        />
+                    })
+                }
             </div>
         </div>
     )
