eddad8a6c289d0bd8325ab91a4e0816c64cb2710
Abhishek <Abhishekraut8697@gmail.com>
2025-08-18 03:19:44 +0530

    added boiler plate code

--- toggleButton/src/App.tsx
+++ toggleButton/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+// ✅ Complete the custom hook below
+// function useToggle(initialValue = false) {
+//   // your code here
+// }
+
+export default function App() {
+  // ✅ Use the custom hook inside this component
+  // const [isOn, toggle] = useToggle(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    /* your toggle function for the onClick method */
+    <button data-testid="toggle-button">
+      {/* Render "ON" or "OFF" based on state */}
+    </button>
+  );
 }
-
-export default App
