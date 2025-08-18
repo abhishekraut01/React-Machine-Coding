bf54baba849deb176b4ff8ecfc7d3cc23745ef2b
Abhishek <Abhishekraut8697@gmail.com>
2025-08-18 16:57:59 +0530

    added bg color fn

--- toggleButton/src/App.tsx
+++ toggleButton/src/App.tsx
@@ -3,9 +3,15 @@ import useToggle from './hooks/ToggleHook'
 
 export default function App() {
   const [isOn , toggle] = useToggle(false)
-
   return (
-    <div>
+    <div style={{
+      backgroundColor:`${isOn ? "black" : "white"}`,
+      height:"100vh" ,
+      width:"100vw",
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center"
+    }}>
       <button data-testid="toggle-button" onClick={toggle}>
         {isOn ? "ON" : "OFF"}
       </button>
