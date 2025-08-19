1391015b16c1724d2b7c0460c873a6053e2c9f57
Abhishek <Abhishekraut8697@gmail.com>
2025-08-20 00:56:46 +0530

    solution partially completed

--- color-explorer/src/App.tsx
+++ color-explorer/src/App.tsx
@@ -1,26 +1,68 @@
-import { IoSearchOutline } from "react-icons/io5";
-
+import { useState, type ChangeEvent } from "react";
+import { colorNameToHex } from "./colorData";
 
 function App() {
+
+  const [inputText, setInputText] = useState<string>("")
+  const [currentColor, setCurrentColor] = useState<string>("")
+  const [hexValue, setHexValue] = useState<string>("")
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value)
+  }
+
+  const handleSearch = () => {
+   
+    if (inputText.trim()) {
+      const colorInSmallLetter = inputText.trim().toLocaleLowerCase()
+      const hex = colorNameToHex(colorInSmallLetter)
+
+      if (hex) {
+        setCurrentColor(inputText.trim());
+        setHexValue(hex);
+      } else {
+        // Handle invalid color
+        setCurrentColor("Invalid Color");
+        setHexValue("N/A");
+      }
+    }
+  }
+
+  const handleOnKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSearch()
+    }
+  }
+
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="h-[500px] w-[400px] flex flex-col justify-center items-center bg-[#F0F0F0] gap-10 p-6  rounded-lg">
+      <div className="h-[500px] w-[400px] flex flex-col justify-center shadow-xl items-center bg-[#F0F0F0] gap-10 p-6  rounded-lg">
         <h1 className="text-4xl text-green-900 font-bold" >color explorer</h1>
 
         <div className="flex   gap-3">
-          <input className="border-1 py-2 px-12 rounded-md bg-white font-semibold" type="text" placeholder="Type color name ex. Red" />
-          <div className="bg-white py-3 px-4 rounded-md flex justify-center items-center ">
-            <IoSearchOutline className="text-xl" />
-          </div>
+          <input
+            value={inputText}
+            onChange={(e) => handleInputChange(e)}
+            onKeyDown={handleOnKeyPress}
+            className="border-1 py-2 px-5 w-70 rounded-md bg-white font-semibold" type="text" placeholder="Type a color name e.g. lavender" />
+          <button
+            onSubmit={handleSearch}
+            className="bg-white py-2 px-3 rounded-md flex justify-center items-center hover:scale-105 ">
+            <h1 className="text-xl cursor-pointer" >🔍</h1>
+          </button>
         </div>
 
         <div className="h-[100%] w-[100%] flex shadow-2xl flex-col justify-center items-center gap-3 bg-white  rounded-md hover:scale-101 transition-all  ">
-          <div className="h-[150px] w-[150px] shadow-sm  rounded-full border-4 border-[#F0F0F0]">
+          <div
+            style={{
+              backgroundColor: hexValue && hexValue !== "N/A" ? hexValue : '#f3f4f6'
+            }}
+            className={`h-[150px] w-[150px] shadow-sm rounded-full border-4 border-[#F0F0F0]`}>
 
           </div >
           <div className="font-bold text-xl">
-            <h2 >Name :</h2>
-            <h3>Hex : </h3>
+            <h2 > Name : {currentColor}</h2>
+            <h3>Hex :{hexValue} </h3>
           </div>
         </div>
       </div>
