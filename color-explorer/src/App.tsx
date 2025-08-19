855f1d0cdb0e327ca7465c2d975b3b2e888064ba
Abhishek <Abhishekraut8697@gmail.com>
2025-08-19 23:59:38 +0530

    ui done

--- color-explorer/src/App.tsx
+++ color-explorer/src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { IoSearchOutline } from "react-icons/io5";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+     <div className="h-[500px] w-[400px] flex flex-col justify-center items-center bg-[#F0F0F0] gap-10 p-6  rounded-lg">
+      <h1 className="text-4xl text-green-900 font-bold" >color explorer</h1>
+
+      <div className="flex   gap-3">
+        <input className="border-1 py-2 px-12 rounded-md bg-white font-semibold" type="text"  placeholder="Type color name ex. Red"/>
+        <div className="bg-white py-3 px-4 rounded-md flex justify-center items-center ">
+          <IoSearchOutline className="text-xl" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="h-[100%] w-[100%] bg-white  rounded-md ">
+        hello
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     </div>
+    </div>
   )
 }
 
