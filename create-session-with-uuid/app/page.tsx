9938d3f47f1dce92326c5db4aa4a0c3023cd96b1
Abhishek <Abhishekraut8697@gmail.com>
2025-08-24 23:45:56 +0530

    creating room logic

--- create-session-with-uuid/app/page.tsx
+++ create-session-with-uuid/app/page.tsx
@@ -1,7 +1,32 @@
+import { useRouter } from 'next/router';
+import { ChangeEvent, useState } from "react";
+
 export default function Home() {
+  const [inputText, setInputText] = useState("")
+  const router = useRouter()
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value)
+  }
+
+  const handleClick = ()=>{
+    router.push('/session');
+  }
+
+  const createRoomId
+
   return (
     <div>
-      
+      <input
+        onChange={(e) => handleInputChange(e)}
+        className="px-3 py-2 rounded-md"
+        value={inputText}
+        type="text" />
+      <button 
+      className='bg-purple-400 px-3 py-2 rounded-2xl'
+      onClick={handleClick}
+      >
+        Join Session
+      </button>
     </div>
   );
 }
