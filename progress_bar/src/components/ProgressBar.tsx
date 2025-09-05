9ff8643ae0b6f8583283565aa9f188c598eaa0c1
Abhishek <Abhishekraut8697@gmail.com>
2025-09-05 23:59:00 +0530

    adding onclick handler

--- progress_bar/src/components/ProgressBar.tsx
+++ progress_bar/src/components/ProgressBar.tsx
@@ -2,7 +2,20 @@ import { useState } from 'react';
 import '../App.css'
 
 function ProgressBars() {
-const [progress , setProgress] = useState(0)
+    const [progress, setProgress] = useState(0)
+
+    const handleIncreament = () => {
+        setProgress((prev) => {
+            if(prev === 100){
+                prev + 10
+            }
+        })
+    }
+
+    const handleDecreament = () => {
+        setProgress((prev) => prev - 10)
+    }
+
     return (
         <div className='ProgressBars-box'>
             <h1>Progress bar</h1>
@@ -10,8 +23,8 @@ const [progress , setProgress] = useState(0)
                 {`${progress}%`}
             </div>
             <div className='Button-div'>
-                <button>-10%</button>
-                <button>+10%</button>
+                <button onClick={handleDecreament}>-10%</button>
+                <button onClick={handleIncreament}>+10%</button>
             </div>
         </div>
     );
