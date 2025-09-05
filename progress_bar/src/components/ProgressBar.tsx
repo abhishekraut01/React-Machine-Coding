209b736e97f8936ea8e48df3f3412c9adec32764
Abhishek <Abhishekraut8697@gmail.com>
2025-09-06 00:40:09 +0530

    complte progress bar project

--- progress_bar/src/components/ProgressBar.tsx
+++ progress_bar/src/components/ProgressBar.tsx
@@ -1,57 +1,38 @@
 import { useEffect, useState } from 'react';
-import '../App.css'
+import '../App.css';
 
 function ProgressBars() {
-    const [progress, setProgress] = useState(0)
-    const [color, setColor] = useState("")
+  const [progress, setProgress] = useState(0);
+  const [color, setColor] = useState('');
 
+  useEffect(() => {
+    if (progress <= 30) setColor('red');
+    else if (progress <= 70) setColor('orange');
+    else setColor('green');
+  }, [progress]);
 
+  const handleIncrement = () => setProgress(prev => Math.min(prev + 10, 100));
+  const handleDecrement = () => setProgress(prev => Math.max(prev - 10, 0));
 
-    useEffect(() => {
-        function handlerColorChange() {
-            if (progress <= 30) {
-                setColor("red")
-            } else if (progress > 30 && progress <= 70) {
-                setColor("orange")
-            } else if (progress > 70) {
-                setColor("green")
-            }
-        }
-        handlerColorChange()
-    }, [progress])
-
-    const handleIncreament = () => {
-        setProgress((prev) => {
-            if (prev < 100) {
-                return prev + 10
-            } else {
-                return 100
-            }
-        })
-    }
-
-    const handleDecreament = () => {
-        setProgress((prev) => {
-            if (prev > 0) {
-                return prev - 10
-            } else {
-                return 0
-            }
-        })
-    }
-
-    return (
-        <div className='ProgressBars-box'>
-            <h1>Progress bar</h1>
-            <div className='testBgColor'>
-                {`${progress}%`}
-            </div>
-            <div className='Button-div'>
-                <button onClick={handleDecreament}>-10%</button>
-                <button onClick={handleIncreament}>+10%</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="ProgressBars-box">
+      <h1>Progress bar</h1>
+      <div className="progressBarParent">
+        <div
+          className="progressFill"
+          style={{
+            width: `${progress}%`,
+            backgroundColor: color,
+          }}
+        />
+        <span className="progressText">{progress}%</span>
+      </div>
+      <div className="Button-div">
+        <button onClick={handleDecrement}>-10%</button>
+        <button onClick={handleIncrement}>+10%</button>
+      </div>
+    </div>
+  );
 }
 
 export default ProgressBars;
