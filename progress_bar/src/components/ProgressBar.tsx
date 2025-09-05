337ffefda9467fbf75f0182d572f02124d932f66
Abhishek <Abhishekraut8697@gmail.com>
2025-09-05 23:53:29 +0530

    ui complete

--- progress_bar/src/components/ProgressBar.tsx
+++ progress_bar/src/components/ProgressBar.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import '../App.css'
 
 function ProgressBars() {
+const [progress , setProgress] = useState(0)
     return (
         <div className='ProgressBars-box'>
-            
+            <h1>Progress bar</h1>
+            <div className='testBgColor'>
+                {`${progress}%`}
+            </div>
+            <div className='Button-div'>
+                <button>-10%</button>
+                <button>+10%</button>
+            </div>
         </div>
     );
 }
